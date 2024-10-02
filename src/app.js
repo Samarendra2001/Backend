23bc8765ca6dafdddec170b60b79eaab66d75327
Samrendra Barick <baricksamarendra@gmail.com>
2024-10-02 21:47:21 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -8,9 +8,19 @@ app.use((req,res)=>{
 })
 
 app.use("/test",(req,res)=>{
-    res.send("Hello from Server");//this will give response from /test route only.
+    res.send("Hello from Server");//this will give response from /test route only.Only if it is the first route
 })
 
+app.use("/hello/2",(req,res)=>{
+    res.send("Hello Sam")
+})
+app.use("/hello/",(req,res)=>{
+    res.send("Hello Samar")
+})
+
+//the above two rotes will only work if it is in this order if the second route "/hello" put first and "/heelo/2" routes put after that then response
+//will only come from "/hello" only. So What am I trying to say is order of routes matter a lot.
+//thi is only because we are using app.use(). It is not in get , post.
 app.listen(3000,()=>{
     console.log("Hi there");
 });
